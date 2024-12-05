63c716cc8282d8f30052884b249745510e981b93
Albilaga Linggra Pradana <git@albilaga.id>
2024-12-05 13:06:36 +0700

    Include Symbols

--- copy-nugets.sh
+++ copy-nugets.sh
@@ -3,7 +3,9 @@ projects=$(cat ./projects.txt)
 
 mkdir -p nugetoutput
 rm nugetoutput/*.nupkg
+rm nugetoutput/*.snupkg
 
 for project in ${projects}; do
 	cp ${project}/nugetoutput/*.nupkg ./nugetoutput/
+	cp ${project}/nugetoutput/*.snupkg ./nugetoutput/
 done
